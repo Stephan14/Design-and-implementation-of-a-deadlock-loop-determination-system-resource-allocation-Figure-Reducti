fe89df4942879ab7b02cf12c0200832f72b4fa03
Stephan14 <zsf657135507@gmail.com>
2016-03-10 21:14:49 +0800

    客户端代码

---
+++ src/com/winnie/client/Client.java
@@ -0,0 +1,10 @@
+package com.winnie.client;
+
+import com.winnie.interfaces.MainBody;
+import com.winnie.view.MainBodyImpl;
+
+public class Client {
+	public static void main(String[] args) {
+		MainBody main = new MainBodyImpl(null);
+	}
+}
