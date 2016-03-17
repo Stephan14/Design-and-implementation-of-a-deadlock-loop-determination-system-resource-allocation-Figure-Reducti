da628035fd215a549e6c53277079759a203cc443
Stephan14 <zsf657135507@gmail.com>
2016-03-17 22:49:17 +0800

    test

---
+++ src/com/winnie/service/JustTest.java
@@ -0,0 +1,9 @@
+package com.winnie.service;
+
+import javax.swing.JOptionPane;
+
+public class JustTest {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "asfd","",JOptionPane.INFORMATION_MESSAGE);
+	}
+}
